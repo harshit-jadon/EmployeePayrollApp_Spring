11a19baee65f24482b0dca76c07b34d73e265ae1
harshit.jadon <harshit.jadon@MD-CJDKE06555KH.magicsw.com>
2021-07-27 00:57:45 +0530

    Welcome to Employee Payroll App in Spring

---
+++ src/main/java/com/bridgelabz/employepayrollapp/EmployePayrollAppApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.employepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployePayrollAppApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Payroll");
+        SpringApplication.run(EmployePayrollAppApplication.class, args);
+    }
+
+}
